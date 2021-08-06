c2be9bab6ae5504b0c69d5b8d347368d5c484672
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-08-06 11:01:13 +0530

    Python conversions file

---
+++ conversions/conversions/conversions.py
@@ -0,0 +1,21 @@
+#Conversion of Data Types
+
+#float to int
+g = 9.8
+int_g = int(g)
+print(int_g)
+
+#int to float
+a = 4.0
+dec_a = float(a)
+print(dec_a)
+
+#string to int
+word = "10008"
+int_word = int(word)
+print(int_word)
+
+#int to string
+x = 10000
+x_str = str(x)
+print(x_str)
